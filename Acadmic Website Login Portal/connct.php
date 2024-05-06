1d9e9575fcd541dc76f971800f6ab6b6e5fa5dd3
Dhruv <152245651+oyedhruvv@users.noreply.github.com>
2024-05-06 19:06:37 +0530

    Update connct.php

--- Acadmic Website Login Portal/connct.php
+++ Acadmic Website Login Portal/connct.php
@@ -11,3 +11,7 @@ if (mysqli_connect_error()) {
     echo "can't connect";
     exit();
 }
+// db name ->  registerform & documents
+// table name -> {new_registration} || {images}
+// coloumn name -> {firstname, lastname, emailfield, passwor,} || {id , image}
+// Run on Xampp.
